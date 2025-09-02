6d3c4e0699c11d7768b9b5da5ad82c65505bbd46
IaraKrasnoff <iara.sf@hotmail.com>
2025-09-01 23:55:41 -0400

    Activate React and final changes

--- OnesToManys/backend/sample_data.sql
+++
@@ -1,3 +0,0 @@
--- Sample data for OnesToManys project
-INSERT INTO master (id, name) VALUES (1, 'Master 1'), (2, 'Master 2');
-INSERT INTO detail (id, master_id, description) VALUES (1, 1, 'Detail 1 for Master 1'), (2, 1, 'Detail 2 for Master 1'), (3, 2, 'Detail 1 for Master 2');
--- OnesToManys/frontend/README.md
+++
@@ -1,9 +0,0 @@
-# Frontend
-
-This folder is for the React frontend application. To initialize, run:
-
-```
-npx create-react-app .
-```
-
-Then build your components to interact with the backend REST API.
